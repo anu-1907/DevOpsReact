bfd64802035f2df5121a8d279b59167ff004e767
--system <anupam.mishra@gigatorb.com>
2022-03-12 14:52:20 +0530

    Description changes

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,11 @@ const Description = () => {
       This is my <span className="italic">First Application</span> using React
       JS. <br /> <br />
       Although it is a very tiny app it still describes how to create components
-      in React, an awesome Javascript Library made by Facebook
+      in React, an awesome Javascript Library made by Facebook.
+      
+      This change is the part of the DevOps pet project.
+      Keep it going guys!
+
     </div>
   )
 }
